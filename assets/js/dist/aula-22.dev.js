1c0a1bff31ed55712eb406430cae2ae0ed5e76b0
magreloos <paulo.hsilva@outlook.com.br>
2025-07-11 00:19:37 -0300

    funções recurciva em javaScript

---
+++ assets/js/dist/aula-22.dev.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var fatorial = function fatorial(valor) {
+  if (valor === 0 || valor === 1) {
+    return 1;
+  }
+
+  return valor * fatorial(valor - 1);
+};
+
+console.log(fatorial(21));
