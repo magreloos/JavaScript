3d01d531a6333f374b97cd3d8f7a5bc646530300
magreloos <paulo.hsilva@outlook.com.br>
2025-07-14 23:59:51 -0300

    comando if e else ou if else

---
+++ assets/js/aula-14.js
@@ -0,0 +1,6 @@
+let num = 11;
+
+if (num > 10)
+  console.log("Numeral maior que 10"), console.log("oi"), console.log(num);
+else console.log("2"), console.log(num);
+console.log("Fim do Programa");
