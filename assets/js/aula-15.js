87c58b61432cabb9bb7b4ae6ba8afba2799acf1e
magreloos <paulo.hsilva@outlook.com.br>
2025-07-15 01:04:51 -0300

    case

---
+++ assets/js/aula-15.js
@@ -0,0 +1,20 @@
+let colocacao = 4;
+
+switch (colocacao) {
+  case 1:
+    console.log("01º Lugar");
+    break;
+  case 2:
+    console.log("02º Lugar");
+    break;
+  case 3:
+    console.log("03º Lugar");
+    break;
+  case 4:
+  case 5:
+  case 6:
+    console.log("Premio Participação");
+    break;
+  default:
+    console.log("Perdeu!");
+}
---
+++ assets/js/dist/aula-15.dev.js
@@ -0,0 +1,26 @@
+"use strict";
+
+var colocacao = 4;
+
+switch (colocacao) {
+  case 1:
+    console.log("01º Lugar");
+    break;
+
+  case 2:
+    console.log("02º Lugar");
+    break;
+
+  case 3:
+    console.log("03º Lugar");
+    break;
+
+  case 4:
+  case 5:
+  case 6:
+    console.log("Premio Participação");
+    break;
+
+  default:
+    console.log("Perdeu!");
+}
